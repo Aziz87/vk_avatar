6f67a9f56060d3c604a2d9ea40a78d3ec1ee1e86
Aziz87 <mailter@mail.ru>
2019-10-20 01:40:21 +0300

    Updates

---
+++ precache-manifest.7f528136d16f5799a132a11157d10dc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1887bacdfec1300ddb9fa5c10fc59528",
+    "url": "/vk_avatar/index.html"
+  },
+  {
+    "revision": "f57dc140d7ea7a8c6b92",
+    "url": "/vk_avatar/static/css/2.7214840c.chunk.css"
+  },
+  {
+    "revision": "8a48bd474be87e29a1b6",
+    "url": "/vk_avatar/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f57dc140d7ea7a8c6b92",
+    "url": "/vk_avatar/static/js/2.9e036cbe.chunk.js"
+  },
+  {
+    "revision": "8a48bd474be87e29a1b6",
+    "url": "/vk_avatar/static/js/main.1337dcfd.chunk.js"
+  },
+  {
+    "revision": "32234272befdae55de2e",
+    "url": "/vk_avatar/static/js/runtime-main.7c3e8512.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk_avatar/static/media/persik.4e1ec840.png"
+  }
+]);
