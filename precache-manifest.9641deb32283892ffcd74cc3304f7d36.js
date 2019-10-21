4bd45c81f6db132c8b213ce3ceb6711f8a2cf339
Aziz87 <mailter@mail.ru>
2019-10-21 04:53:56 +0300

    Updates

--- precache-manifest.9641deb32283892ffcd74cc3304f7d36.js
+++ precache-manifest.9641deb32283892ffcd74cc3304f7d36.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ab64654f139044cfb26465bfee74938",
+    "revision": "aed4b0546ee728d439988f3d83dc23f4",
     "url": "/vk_avatar/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vk_avatar/static/css/2.7214840c.chunk.css"
   },
   {
-    "revision": "d2c6bc0b8c50e0f245e8",
+    "revision": "34a42834dbeac3b5ac67",
     "url": "/vk_avatar/static/css/main.981f7211.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vk_avatar/static/js/2.c9499849.chunk.js"
   },
   {
-    "revision": "d2c6bc0b8c50e0f245e8",
-    "url": "/vk_avatar/static/js/main.e0fbd075.chunk.js"
+    "revision": "34a42834dbeac3b5ac67",
+    "url": "/vk_avatar/static/js/main.157eae5a.chunk.js"
   },
   {
     "revision": "32234272befdae55de2e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk_avatar/precache-manifest.adf15dac6fcca934e0446ee676c5530f.js"
+  "/vk_avatar/precache-manifest.9641deb32283892ffcd74cc3304f7d36.js"
 );
 
 self.addEventListener('message', (event) => {
